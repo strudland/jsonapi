5c41b75587e7255176b485251257b8d1ce211da4
Andrej <strudl.ruzca@gmail.com>
2017-10-24 22:33:25 +0000

    added all routes and moved logic to helpers folder

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require("express"),
-    app     = express()
+    app     = express(),
+    bodyParser = require("body-parser");
     
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
     res.send("Hi there!!");
-})
+});
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(process.env.PORT, function(){
     console.log("APP IS RUNNING ON PORT" + process.env.PORT);
